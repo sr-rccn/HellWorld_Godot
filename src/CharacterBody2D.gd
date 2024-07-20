147243b861ae21c637040e93b173a7f6e108ea7e
Sergio Rivas <tatan_bill@hotmail.com>
2024-07-20 00:13:54 -0400

    Refactoring proyect

--- src/CharacterBody2D.gd
+++ src/CharacterBody2D.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 @onready var _animated_sprite = $AnimatedSprite2D
 @onready var _animation_player = $AnimatedSprite2D/AnimationPlayer
 @onready var _sword_shape = $AnimatedSprite2D/Area2D/CollisionShape2D
+@onready var jumper = $"../Jumper/CollisionShape2D"
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -200.0
@@ -32,19 +33,45 @@ func _physics_process(delta):
 		jumps = jumps + 1
 		velocity.y = JUMP_VELOCITY
 		
-		
 		if direction == 1:
 			_animated_sprite.play("jump_right")
 		if direction == -1:
 			_animated_sprite.play("jump_left")
+			
 		# Double jump
 		if jumps < 1:
 			jumps = jumps + 1
 			velocity.y = JUMP_VELOCITY
 	
 	
-	#Controla animación al caer del salto
 	
+	on_floor_and_jump(direction)
+	move_and_jump_left(direction)
+	move_and_jump_right(direction)
+	attack_left(direction)
+	attack_right(direction)
+	move_character(direction)
+	save_last_movement(direction)
+	cancel_attack_animation()
+	box_collision()
+	
+func on_floor_and_jump(direction):	
+	if direction == 0 and !is_on_floor():
+		if last_movement == -1:
+			_animated_sprite.play("jump_left")
+		if last_movement == 1:
+			_animated_sprite.play("jump_right")
+	
+func move_and_jump_left(direction):
+	if direction == -1 and !is_on_floor():
+		_animated_sprite.play("jump_left")
+
+	
+func move_and_jump_right(direction):
+	if direction == 1 and !is_on_floor():
+		_animated_sprite.play("jump_right")
+	
+func attack_right(direction):
 	#Mover y saltar a la derecha
 	if last_movement == 1 and direction == 0 and is_on_floor():
 		if Input.is_action_just_pressed("ui_attack"):
@@ -57,22 +84,11 @@ func _physics_process(delta):
 				if rigid_body is RigidBody2D:
 					rigid_body.apply_central_impulse(Vector2(90, 0))
 
-					#rigid_body.queue_free()
-					print(rigid_body.mass)
-			#move boxes
-
-			#for area in overlapping:
-				#var parent = area.get_parent()
-				#parent.queue_free()
-
 		else:
 			if _animated_sprite.animation != "attack_right":
 				_animated_sprite.play("stand_right")	
 				
-	if direction == 1 and !is_on_floor():
-		_animated_sprite.play("jump_right")
-
-		
+func attack_left(direction):
 	#Mover y saltar a la izquierda
 	if last_movement == -1 and direction == 0 and is_on_floor():
 		if Input.is_action_just_pressed("ui_attack"):
@@ -84,59 +100,49 @@ func _physics_process(delta):
 				if rigid_body is RigidBody2D:
 					rigid_body.apply_central_impulse(Vector2(-90, 0))
 
-					#rigid_body.queue_free()
 					print(rigid_body.mass)
-			#for area in overlapping:
-				#var parent = area.get_parent()
-				#parent.queue_free()
 			#
 		else:
 			if _animated_sprite.animation != "attack_left":
 				_animated_sprite.play("stand_left")
 				
-	if direction == -1 and !is_on_floor():
-		_animated_sprite.play("jump_left")
 	
-	if direction == 0 and !is_on_floor():
-		if last_movement == -1:
-			_animated_sprite.play("jump_left")
-		if last_movement == 1:
-			_animated_sprite.play("jump_right")
-	
-	
-	if Input.is_action_pressed("ui_right") and is_on_floor():
-		_animated_sprite.play("run_right")
-	if Input.is_action_pressed("ui_left") and is_on_floor():
-		_animated_sprite.play("run_left")
+
+func move_character(direction):
+		if direction:
+			velocity.x = (direction * SPEED)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+		if Input.is_action_pressed("ui_right") and is_on_floor():
+			_animated_sprite.play("run_right")
+		if Input.is_action_pressed("ui_left") and is_on_floor():
+			_animated_sprite.play("run_left")
 		
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
 
-
-	#Cancela animación despues de atacar
+func cancel_attack_animation():
+		#Cancela animación despues de atacar
 	if _animated_sprite.is_playing() == false and _animated_sprite.animation.contains("attack"):
 		_animated_sprite.stop()
+		
 		if last_movement == 1:
 			_animated_sprite.play("stand_right")
 		if last_movement == -1:
 			_animated_sprite.play("stand_left")
 
-
+func save_last_movement(direction):
 	#Guarda el último movimiento si fue derecha o izquierda
 	if(direction != 0):
-		last_movement = direction
-		
+		last_movement = direction		
 
+
+func box_collision():
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var collider = collision.get_collider()
 		if collider is RigidBody2D:
 			var impulse = -collision.get_normal()
-			print(impulse)
 			collider.apply_central_impulse(impulse)
-			
-	move_and_slide()
 
-
+			if collider.name.contains("Jumper"): velocity.y = JUMP_VELOCITY * 2
