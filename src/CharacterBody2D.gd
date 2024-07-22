2120669e2437a6cb01d0f0763ba47446b153681b
Sergio Rivas <tatan_bill@hotmail.com>
2024-07-22 05:42:33 -0400

    FEAT: slide on wall animation

--- src/CharacterBody2D.gd
+++ src/CharacterBody2D.gd
@@ -31,25 +31,14 @@ func _physics_process(delta):
 	if !is_on_floor() and jumps > 1:
 		has_double_jump = false
 		
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and has_double_jump :
-		jumps = jumps + 1
-		velocity.y = JUMP_VELOCITY
-		
-		if direction == 1:
-			_animated_sprite.play("jump_right")
-		if direction == -1:
-			_animated_sprite.play("jump_left")
-			
-		# Double jump
-		if jumps < 1:
-			jumps = jumps + 1
-			velocity.y = JUMP_VELOCITY
-	
-
-	#print([_animated_sprite.animation, _animated_sprite.is_playing()])
+	if is_on_wall():
+		velocity = velocity / 2
+		has_double_jump = true
 
 	idle(direction)
+	
+	wall_collision(direction)
+	handle_jump(direction)
 	on_floor_and_down()
 	attack_left(direction)
 	attack_right(direction)
@@ -63,30 +52,29 @@ func _physics_process(delta):
 
 			
 func idle(direction):
-	var animation_playing = _animated_sprite.is_playing()
+	#var animation_playing = _animated_sprite.is_playing()
 	var animation_type = _animated_sprite.animation
 	var button_down_pressed = Input.is_action_pressed("ui_down", true)
-	print([animation_type, animation_playing])
 	
 	#is_playing_by_name(_animated_sprite, "sit_up_right")
 
 	#print(!button_down_pressed and is_playing_by_name(_animated_sprite, "sit_up_right"))
-	if(is_playing_by_name(_animated_sprite, "sit_up")):
+	if(is_playing_by_name("sit_up")):
 		return #
 		
 	if is_on_floor() and direction == 0 and !button_down_pressed:
 		if animation_type.contains("jump") :
-			stand(direction, last_movement)
+			stand(direction)
 				
 		if !animation_type.contains("attack"):
-			stand(direction, last_movement)
+			stand(direction)
 				
-		if !is_playing_by_name(_animated_sprite, "attack"):
-			stand(direction, last_movement)
+		if !is_playing_by_name("attack"):
+			stand(direction)
 
 func cancel_attack_animation():
 		#Cancela animación despues de atacar
-	if is_playing_by_name(_animated_sprite, "attack"):
+	if is_playing_by_name("attack"):
 		_animated_sprite.stop()
 		
 		if last_movement == 1:
@@ -97,7 +85,6 @@ func cancel_attack_animation():
 func on_floor_and_down():
 	var button_down_pressed = Input.is_action_pressed("ui_down", true)
 	var button_down_released = Input.is_action_just_released("ui_down", true)
-	print(button_down_released)
 
 	if is_on_floor():
 		if button_down_released:
@@ -106,26 +93,29 @@ func on_floor_and_down():
 			
 		if button_down_pressed:	
 				if last_movement == 1: _animated_sprite.play("sit_down_right")
-				
 				if last_movement == -1: _animated_sprite.play("sit_down_left")
-				#else: _animated_sprite.play("sit_up_right")
-
 	
 func on_floor_and_jump(direction):	
-	if direction == 0 and !is_on_floor():
-		if last_movement == -1:
-			_animated_sprite.play("jump_left")
-		if last_movement == 1:
-			_animated_sprite.play("jump_right")
+	return #
+	#if direction == 0 and !is_on_floor():
+		#if last_movement == -1:
+			#_animated_sprite.play("jump_left")
+		#if last_movement == 1:
+			#_animated_sprite.play("jump_right")
 	
 func move_and_jump_left(direction):
-	if direction == -1 and !is_on_floor():
-		_animated_sprite.play("jump_left")
+	if !is_on_wall():
+		if direction == -1 and !is_on_floor():
+			_animated_sprite.play("jump_left")
 
 	
 func move_and_jump_right(direction):
-	if direction == 1 and !is_on_floor():
-		_animated_sprite.play("jump_right")
+	if !is_on_wall():
+		if direction == 1 and !is_on_floor():
+			_animated_sprite.play("jump_right")
+	#if is_on_wall():
+		#var shape = $CollisionShape2D
+		#shape.appl
 	
 func attack_right(direction):
 	#Mover y saltar a la derecha
@@ -138,14 +128,9 @@ func attack_right(direction):
 			var overlapping = $AnimatedSprite2D/Sword.get_overlapping_bodies()
 			for rigid_body in overlapping:
 				if rigid_body is RigidBody2D:
-					print(rigid_body)
 					if rigid_body.name.contains("Dummy"): 
 						rigid_body.queue_free()
 					rigid_body.apply_central_impulse(Vector2(90, 0))
-
-			#else:
-				#if _animated_sprite.animation != "attack_right":
-					#_animated_sprite.play("stand_right")	
 					
 func attack_left(direction):
 	#Mover y saltar a la izquierda
@@ -163,6 +148,23 @@ func attack_left(direction):
 			#if _animated_sprite.animation != "attack_left":
 				#_animated_sprite.play("stand_left")
 				
+func handle_jump(direction):	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and has_double_jump :
+		jumps = jumps + 1
+		velocity.y = JUMP_VELOCITY
+		
+		if is_on_wall():
+			jumps = 1
+		
+		if direction == 1:
+			_animated_sprite.play("jump_right")
+		if direction == -1:
+			_animated_sprite.play("jump_left")
+			
+		# Double jump
+		if jumps < 1:
+			jumps = jumps + 1
+			velocity.y = JUMP_VELOCITY
 	
 
 func move_character(direction):
@@ -190,23 +192,28 @@ func save_last_movement(direction):
 func box_collision():
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
+		#print(collision)
 		var collider = collision.get_collider()
 		if collider is RigidBody2D:
 			var impulse = -collision.get_normal()
 			collider.apply_central_impulse(impulse)
 			if collider.name.contains("Jumper"): velocity.y = JUMP_VELOCITY * 2
 
-
-
+func wall_collision(direction):
+	if is_on_wall() and direction == 1:
+		_animated_sprite.play("wall_slide_right")
+	if is_on_wall() and direction == -1:
+		_animated_sprite.play("wall_slide_left")
+	
 func _on_area_2d_body_entered(body):
 	if body is CharacterBody2D:
 		body.get_tree().call_deferred("reload_current_scene")
 		
-func stand(direction, last_movement):
+func stand(direction):
 	if direction == 0 and last_movement == 1:
 		_animated_sprite.play("stand_right")
 	if direction == 0 and last_movement == -1:
 		_animated_sprite.play("stand_left")
 		
-func is_playing_by_name(_animated_sprite, name):
-	return _animated_sprite.is_playing() == true and _animated_sprite.animation.contains(name)
+func is_playing_by_name(animation_type):
+	return _animated_sprite.is_playing() == true and _animated_sprite.animation.contains(animation_type)
