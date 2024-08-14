81adcb9b4bd1618ab2ea02272b3f924b4e857ed0
Sergio Rivas <tatan_bill@hotmail.com>
2024-08-13 23:34:18 -0400

    feat: change to level2

---
+++ src/scenes/scenarios/EndLevel.gd
@@ -0,0 +1,22 @@
+extends Area2D
+var simultaneous_scene = preload("res://src/scenes/scenarios/Level2.tscn").instantiate()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	print(body)
+	print(body.name.contains("Kara"))
+	if body.name.contains("Kara"):
+		get_tree().change_scene_to_file("res://src/scenes/scenarios/Level2.tscn")
+		#get_tree().root.add_child(simultaneous_scene)
+
+
